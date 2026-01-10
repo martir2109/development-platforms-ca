0b991086d7482fd6452a90b6d2feb0c199d78baa
Martine R Karlsen <martinekar08@gmail.com>
2026-01-10 20:14:34 +0100

    Config tailwind, vite, prettier, readme

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # development-platforms-ca
+
+# Getting started
+
+## Install dependencies
+
+```bash
+npm install
+```
+
+## Run the project (development)
+
+Run the development server with Vite:
+
+```bash
+npm run dev
+```
+
+## Build the project
+
+```bash
+npm run build
+```
+
+## Preview the project
+
+```bash
+npm run preview
+```
+
+## Format all files with prettier
+
+```bash
+npm run prettier
+```
---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
+
+export default defineConfig({
+  plugins: [tailwindcss()],
+  server: {
+    port: 5501,
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "./index.html"),
+        login: resolve(__dirname, "./auth/login/index.html"),
+        register: resolve(__dirname, "./auth/register/index.html"),
+      },
+    },
+  },
+});
