136b897a5994302aab626ce08ec4c4f0a9e3de58
Martine R Karlsen <martinekar08@gmail.com>
2026-01-14 21:44:49 +0100

    Supabase configuration

---
+++ src/js/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
+
+const supabaseUrl = "https://iwbnvybjoiuyxanrjsir.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml3Ym52eWJqb2l1eXhhbnJqc2lyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgyMTk4NTMsImV4cCI6MjA4Mzc5NTg1M30.OgmoUS3Xp1e7NJ94tS5P39V9ENNC18VMFOk6ZnfewwY";
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
