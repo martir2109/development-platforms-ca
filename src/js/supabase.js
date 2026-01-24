fffe4f5903758483571cb72cec1de9bd8ba3d53f
Martine R Karlsen <martinekar08@gmail.com>
2026-01-24 17:46:12 +0100

    added correct import createClient from
    As written in the Supabase docs: https://supabase.com/docs/reference/javascript/installing

--- src/js/supabase.js
+++ src/js/supabase.js
@@ -1,4 +1,4 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
+import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://iwbnvybjoiuyxanrjsir.supabase.co";
 const supabaseKey =
